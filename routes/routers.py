c13fea248662401422630091726c971d1ead331d
igorwilliam <igorwm9@gmail.com>
2018-04-26 16:32:15 -0300

    pprint

--- routes/routers.py
+++ routes/routers.py
@@ -218,7 +218,7 @@ def getIdGrupo(idgrupo):
 def teste():
 
     if (request.method == 'POST'):
-        print (request.json)
+        pprint (request.json)
         res = user.teste(request.json)
         return dumps(res)
 
