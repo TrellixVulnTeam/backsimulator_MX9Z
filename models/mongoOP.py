1667b2f2df1ca5f9609ef490b029a8a5afcf2524
igorwilliam <igorwm9@gmail.com>
2018-04-26 15:26:16 -0300

    insert dados

--- models/mongoOP.py
+++ models/mongoOP.py
@@ -28,7 +28,7 @@ class OpMongoDB():
         response = {}
 
         try:
-            response = self.collection.insert_one(response)
+            response = self.collection.insert(dados)
             # self.collection.insert_one({"dados":1})
             reseponse = {
                 "hi":"dados"
